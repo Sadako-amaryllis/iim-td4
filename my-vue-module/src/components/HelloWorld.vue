7c00b5d04829cdea7194c06d75f5de716e2ce3d1
Mylene <mylene.mzali@edu.devinci.fr>
2023-07-04 11:32:33 +0200

    number aleatoire

--- my-vue-module/src/components/HelloWorld.vue
+++ my-vue-module/src/components/HelloWorld.vue
@@ -1,35 +1,23 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-    </p>
+  <div>
+    <p>Nombre aléatoire: {{ randomNumber }}</p>
+    <button @click="generateRandomNumber">Générer un nouveau chiffre aléatoire</button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import getRandomNumber from '../../../index';
 
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  data() {
+    return {
+      randomNumber: null,
+    };
+  },
+  methods: {
+    generateRandomNumber() {
+      this.randomNumber = getRandomNumber();
+    },
+  },
+};
+</script>
